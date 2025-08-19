89add05db980dea0861c352d182210a454e0fb61
Aravind <aravindbujji7@gmail.com>
2025-08-19 17:29:00 +0530

    Initial Commit Project Creation

---
+++ src/test/java/com/ecom/Security_Service/SecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.Security_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
