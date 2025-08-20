ab49b6c829c242390107f7955312c8ab8882372f
Aravind <aravindbujji7@gmail.com>
2025-08-20 18:29:34 +0530

    modified security filter and added password request

--- src/main/java/com/ecom/Security_Service/config/SecurityConfig.java
+++ src/main/java/com/ecom/Security_Service/config/SecurityConfig.java
@@ -1,28 +1,25 @@
 package com.ecom.Security_Service.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                // Disable CSRF since we use JWT
                 .csrf(csrf -> csrf.disable())
-
-                // Disable default login form
                 .formLogin(form -> form.disable())
-
-                // Disable HTTP Basic Auth
                 .httpBasic(basic -> basic.disable())
 
                 // Authorize requests
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**").permitAll() // allow health checks
-                        .requestMatchers("/validate-token").permitAll() // allow token validation
-                        .requestMatchers("/hash-password").permitAll() // allow password hashing
+                        .requestMatchers("/api/v1/security/validate-token").permitAll() // allow token validation
+                        .requestMatchers("/api/v1/security/hash-password").permitAll() // allow password hashing
                         .anyRequest().authenticated() // everything else requires authentication
                 );
 
---
+++ src/main/java/com/ecom/Security_Service/dto/PasswordRequest.java
@@ -0,0 +1,11 @@
+package com.ecom.Security_Service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PasswordRequest {
+
+   private String password;
+}
