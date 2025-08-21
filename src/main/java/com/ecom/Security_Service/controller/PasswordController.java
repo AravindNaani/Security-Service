8e93541da56bf4202669b3a259a584d3ca4b2219
Aravind <aravindbujji7@gmail.com>
2025-08-21 17:02:34 +0530

    added traces to the endpoints from gateway

--- src/main/java/com/ecom/Security_Service/controller/PasswordController.java
+++ src/main/java/com/ecom/Security_Service/controller/PasswordController.java
@@ -7,13 +7,12 @@ import com.ecom.Security_Service.dto.LoginRequest;
 import com.ecom.Security_Service.dto.PasswordRequest;
 import com.ecom.Security_Service.service.IPasswordService;
 import jakarta.validation.constraints.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -21,18 +20,23 @@ import java.time.LocalDateTime;
 @RequestMapping("/api/v1/security")
 public class PasswordController {
 
+    private static final Logger logger = LoggerFactory.getLogger(PasswordController.class);
+
     @Autowired
     private IPasswordService passwordService;
 
     @PostMapping("/hash-password")
-    public ResponseEntity<ApiResponse> hashPassword(@RequestBody @NotNull PasswordRequest password){
-
+    public ResponseEntity<ApiResponse> hashPassword(@RequestHeader("X-Trace-Id") String TraceId,
+                                                    @RequestBody @NotNull PasswordRequest password) {
+        logger.debug("Received request to hash-password with TraceId: {}", TraceId);
         String hassedPassword = passwordService.hashPassword(password.getPassword());
-        return ResponseEntity.ok(new ApiResponse(Constants.successful, LocalDateTime.now(),hassedPassword));
+        return ResponseEntity.ok(new ApiResponse(Constants.successful, LocalDateTime.now(), hassedPassword));
     }
 
     @PostMapping("/validate-credentials")
-    public ResponseEntity<ApiResponse> validateCredentials(@RequestBody LoginRequest loginRequest){
+    public ResponseEntity<ApiResponse> validateCredentials(@RequestHeader("X-Trace-Id") String TraceId,
+                                                            @RequestBody LoginRequest loginRequest) {
+        logger.debug("Received request to validate-credentials with TraceId: {}", TraceId);
         JwtResponse jwtResponse = passwordService.validateCredentials(loginRequest);
         return ResponseEntity.ok(new ApiResponse("SUCCESSFULL", LocalDateTime.now(), jwtResponse));
     }
